50e0b4c61bb29eb759bf084cd05f687558e95890
canalesf23 <canalesf23@gmail.com>
2024-07-18 08:13:09 -0600

    Agregar archivos de proyecto.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace sumamvvm
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ sumaViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace sumamvvm
+{
+    partial class sumaViewModel:ObservableObject
+    {
+        [ObservableProperty]
+        public string numero1;
+        [ObservableProperty]
+        public string numero2;
+        [ObservableProperty]
+        public string resultado;
+
+        [RelayCommand]
+        private void ActualizarDatos() {
+            if (double.TryParse(Numero1, out double num1) && double.TryParse(Numero2, out double num2))
+            {
+                Resultado = (num1 + num2).ToString();
+            }
+            else
+            {
+                Resultado = "Ingrese números válidos";
+            }
+        }
+
+
+
+    }
+}
